7622dacf22a421272bdf7f904a2d8c6a0a89e2ea
giovannipizzolante <gpizzolante8@gmail.com>
2024-07-16 12:30:03 +0200

    workflows v3

--- tests/test_unit_converter.c
+++ tests/test_unit_converter.c
@@ -10,8 +10,14 @@ void test_celsius_to_fahrenheit() {
   printf("All celsius_to_fahrenheit tests passed!\n");
 }
 
-void test_fahrenheit_to_celsius() {
-  printf("All fahrenheit_to_celsius tests passed!\n");
+void test1_fahrenheit_to_celsius() {
+  assert(fabs(fahrenheit_to_celsius(32) - 0 < EPSILON);
+  printf("First test fahrenheit to celsius passed!\n");
+}
+
+void test2_fahrenheit_to_celsius() {
+  assert(fabs(fahrenheit_to_celsius(32) - 10 < EPSILON);
+  printf("First test fahrenheit to celsius passed!\n");
 }
 
 void test_kilometers_to_miles() {
@@ -28,7 +34,8 @@ void test_ounces_to_grams() { printf("All ounces_to_grams tests passed!\n"); }
 
 int main() {
   test_celsius_to_fahrenheit();
-  test_fahrenheit_to_celsius();
+  test1_fahrenheit_to_celsius();
+  test2_fahrenheit_to_celsius();
   test_kilometers_to_miles();
   test_miles_to_kilometers();
   test_grams_to_ounces();
